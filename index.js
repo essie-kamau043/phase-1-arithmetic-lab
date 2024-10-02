f40c3d0b6971f2abc0b62fd593601ceb6344df5a
essie-kamau043 <essie.k004@gmail.com>
2024-10-02 11:49:47 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=62
+let num2=1
 
-// Write your code here
+let multiply=num1 * num2
+
+let random=Math.ceil(Math.random() *10)
+
+
+let num3=29
+let num4=5
+
+let mod= num3 % num4
+
+let max=Math.max(5,10,15,20);
